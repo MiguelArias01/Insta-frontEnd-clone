96625a5ccc9d3fe93f4af457144762c6d78f34ab
MiguelArias01 <Argenis803@gmail.com>
2023-04-07 10:09:37 -0400

    added names to stories

--- frontend/src/api/mainfeed.js
+++ frontend/src/api/mainfeed.js
@@ -4,3 +4,8 @@ export async function getMainFeed(page) {
   const res = await api.get(`api/home/?page=${page}`);
   return res.data;
 }
+
+export async function getUser(username) {
+  const res = await api.get(`api/users/${username}/`);
+  return res.data;
+}
