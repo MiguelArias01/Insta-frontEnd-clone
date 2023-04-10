eeae2ed62891e177f94347933156829c4832ba4c
Jazsadevee <jazsadevee@gmail.com>
2023-04-10 17:15:03 -0400

    adding and check like and dislike functionality to each post

--- frontend/src/components/Posts/Posts.jsx
+++ frontend/src/components/Posts/Posts.jsx
@@ -1,6 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import { getMainFeed, createComment } from '../../api/mainfeed';
-import like from '../Likes/like.jsx';
+import LikeDislikeButtons from '../Likes/like';
+
+function Post({ post, onSubmit }) {
+  const [comment, setComment] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onSubmit(post.id, comment);
+    setComment('');
+  }
+
+  return (
+    <div className="border rounded-md shadow-md overflow-hidden w-4/12 m-8">
+      <img className="object-cover h-64 w-full" src={post.image} alt={post.caption} />
+      <div className="p-4">
+        <h2 className="text-sm font-sans font-medium mb-2">{post.caption}</h2>
+        <p className="text-gray-600">
+          Author: {post.author.profile.firstName} {post.author.profile.lastName}
+        </p>
+      </div>
+      <LikeDislikeButtons postId={post.id} userId={post.author.id} />
+      <div>
+        {post.comments.map((comment, commentIndex) => (
+          <div className="flex justify-center" key={commentIndex}>
+            <div>{comment.text}</div>
+          </div>
+        ))}
+
+
+        <form onSubmit={handleSubmit}>
+          <input
+            className="w-full py-2 px-4 border border-gray-300 rounded-md"
+            type="text"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            placeholder="Add a comment..."
+          />
+          <button className="w-full py-2 px-4 text-white bg-blue-500 rounded-md hover:bg-blue-600" type="submit">
+            Post comment
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
 
 function Posts() {
   const [data, setData] = useState([]);
@@ -26,20 +70,7 @@ function Posts() {
     }
   }
 
-  function handleScroll() {
-    if (
-      window.innerHeight + document.documentElement.scrollTop ===
-      document.documentElement.offsetHeight &&
-      !endReached
-    ) {
-      fetchData();
-    }
-  }
-
-  async function handleSubmit(e, postIndex, postarray) {
-    e.preventDefault();
-    let postId = postarray[postIndex].id;
-
+  async function handleSubmitComment(postId, commentText) {
     const newComment = {
       comment: commentText,
       post_id: postId,
@@ -67,31 +98,8 @@ function Posts() {
 
   return (
     <div className="flex flex-col items-center">
-      {data.map((item, index, array) => (
-        <div className="border rounded-md shadow-md overflow-hidden w-4/12 m-8" key={index}>
-          <img className="object-cover h-64 w-full" src={item.image} alt={item.caption} />
-          <div className="p-4">
-            <h2 className="text-lg font-medium mb-2">{item.caption}</h2>
-            <p className="text-gray-600">
-              Author: {item.author.profile.firstName} {item.author.profile.lastName}
-            </p>
-          </div>
-
-
-
-
-          <div>
-            {item.comments.map((comment, commentIndex) => (
-              <div className="flex justify-center" key={commentIndex}>
-                {comment.text}
-              </div>
-            ))}
-            <form onSubmit={(e) => handleSubmit(e, index, array)}>
-              <input type="text" value={commentText} onChange={(e) => setCommentText(e.target.value)} placeholder="Add a comment..." />
-              <button className="w-full py-2 px-4 text-white bg-blue-500 rounded-md hover:bg-blue-600" type="submit">Post</button>
-            </form>
-          </div>
-        </div>
+      {data.map((post) => (
+        <Post key={post.id} post={post} onSubmit={handleSubmitComment} />
       ))}
       {endReached && <div className="text-center mt-6">You have reached the end!</div>}
     </div>
