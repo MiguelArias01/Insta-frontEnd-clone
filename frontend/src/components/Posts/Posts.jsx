2d7613065b659afdf47c77760dfafea532884ce3
Jazsadevee <jazsadevee@gmail.com>
2023-04-11 09:04:07 -0400

    changes from dev

--- frontend/src/components/Posts/Posts.jsx
+++ frontend/src/components/Posts/Posts.jsx
@@ -46,46 +46,6 @@ function Post({ post, onSubmit }) {
   );
 }
 
-function Post({ post, onSubmit }) {
-  const [comment, setComment] = useState('');
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    onSubmit(post.id, comment);
-    setComment('');
-  }
-
-  return (
-    <div className="border rounded-md shadow-md overflow-hidden w-4/12 m-8">
-      <img className="object-cover h-64 w-full" src={post.image} alt={post.caption} />
-      <div className="p-4">
-        <h2 className="text-lg font-medium mb-2">{post.caption}</h2>
-        <p className="text-gray-600">
-          Author: {post.author.profile.firstName} {post.author.profile.lastName}
-        </p>
-      </div>
-      <div>
-        {post.comments.map((comment, commentIndex) => (
-          <div className="flex justify-center" key={commentIndex}>
-            <div>{comment.text}</div>
-          </div>
-        ))}
-        <form onSubmit={handleSubmit}>
-          <input
-            className="w-full py-2 px-4 border border-gray-300 rounded-md"
-            type="text"
-            value={comment}
-            onChange={(e) => setComment(e.target.value)}
-            placeholder="Add a comment..."
-          />
-          <button className="w-full py-2 px-4 text-white bg-blue-500 rounded-md hover:bg-blue-600" type="submit">
-            Post comment
-          </button>
-        </form>
-      </div>
-    </div>
-  );
-}
 
 function Posts() {
   const [data, setData] = useState([]);
